23b0b7681430ee04e1affe48624e3370d5983fc4
KUNAL CHOURE <chourekunal4@gmail.com>
2024-12-05 12:48:30 +0530

    backeground added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import { Routes,Route } from 'react-router-dom';
+import Landingpage from './pages/landing'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+   
+   <Routes>
+
+    <Route path="/" element={<Landingpage/>}/>
+
+   </Routes>
    
     </>
   )
