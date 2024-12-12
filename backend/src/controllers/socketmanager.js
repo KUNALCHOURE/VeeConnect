3a2cc7a60a7f148e834c6e67c34ecabccbfaac41
KUNAL CHOURE <chourekunal4@gmail.com>
2024-12-12 22:31:03 +0530

    connecting webrtc

--- backend/src/controllers/socketmanager.js
+++ backend/src/controllers/socketmanager.js
@@ -15,6 +15,8 @@ const connnectToserver = (server) => {
   });
 
   io.on("connection", (socket) => {
+
+    console.log("something connected");
     socket.on("join-call", (path) => {
       if (connections[path] === undefined) {
         connections[path] = [];
